5246d02aee4bdcb59a2de766747a996f9328d605
lopezz <lopezcardona24@gmail.com>
2014-04-22 21:30:04 -0500

    first files created

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from chili_pairs_game import ChiliPairsGame
+
+class ChiliPairsApp(App):
+	def build(self):
+		return ChiliPairsGame()
+
+if __name__ == '__main__':
+	ChiliPairsApp().run()
