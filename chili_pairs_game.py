d46c10af56d23e45bfdb53d08b2297c92a47fa39
lopezz <lopezcardona24@gmail.com>
2014-04-25 00:36:22 -0500

    load cards from .csv file

--- chili_pairs_game.py
+++ chili_pairs_game.py
@@ -1,7 +1,8 @@
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty, NumericProperty
-from chili_card import ChiliImageCard
+from chili_card import ChiliImageCard, ChiliWordCard
+import csv
 
 class ChiliPairsGame(Widget):
 
@@ -13,11 +14,27 @@ class ChiliPairsGame(Widget):
         ''' Sets a new game and starts it '''
         # TODO reset values
         self.chiligrid.clear_widgets()
-        for i in range(4 * 6):
-            #self.chiligrid.add_card(ChiliWordCard(text='Test card'))
-            self.chiligrid.add_card(ChiliImageCard(img='img/pear.png'))
-            #self.chiligrid.add_card(ChiliSoundCard(img='ruta'))
+        self.load_cards()
         self.play()
+
+    def load_cards(self):
+        ''' Reads and loads the cards for the cvs file '''
+        cards= open('card_sets/fruits.csv')
+        read_cards = list(csv.reader(cards))
+        c=0
+        # the set will load 4 times (there's 6 cards in fruits.csv)   
+        for i in range(3):
+            print i
+            for row in read_cards:
+                print i
+                for col in row:
+                    if c==0:
+                        self.chiligrid.add_card(ChiliWordCard(text=col))
+                        c=1
+                    else:
+                        self.chiligrid.add_card(ChiliImageCard(img=col))
+                        c=0
+        cards.close()
 
     def play(self):
         ''' Play/start game '''
