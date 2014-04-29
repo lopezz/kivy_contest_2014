29a20db98e3557fc60acea36601fc13b19751839
lopezz <lopezcardona24@gmail.com>
2014-04-29 12:17:50 -0500

    Fixes #1
    -New member to the Class ChiliPairsGame with the list of sets available.
    -load_cards receives the id of the set picked for the new game.

--- chili_pairs_game.py
+++ chili_pairs_game.py
@@ -1,32 +1,43 @@
+import csv, os, random
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty, NumericProperty
 from chili_card import ChiliImageCard, ChiliWordCard
-import csv
+
 
 class ChiliPairsGame(Widget):
 
     chiligrid = ObjectProperty(None)
+    setlists= list()
     
+    def __init__(self, *args, **kwargs):
+        super(ChiliPairsGame, self).__init__(*args, **kwargs)
+        sets=os.listdir('card_sets') 
+        for i in sets:
+            path=''.join(['card_sets/', i])
+            set_name=i.split('.')[0]
+            self.setlists.append([set_name, path])
+        print self.setlists
+        
 
 
     def new_game(self):
         ''' Sets a new game and starts it '''
         # TODO reset values
         self.chiligrid.clear_widgets()
-        self.load_cards()
+        #pick a random set of cards
+        set_id=random.randint(0,len(self.setlists)-1)
+        self.load_cards(set_id)
         self.play()
 
-    def load_cards(self):
+    def load_cards(self, set_id):
         ''' Reads and loads the cards for the cvs file '''
-        cards= open('card_sets/fruits.csv')
-        read_cards = list(csv.reader(cards))
+        cards= open(self.setlists[set_id][1])
+        read_cards = list(csv.reader(cards))        
         c=0
         # the set will load 4 times (there's 6 cards in fruits.csv)   
-        for i in range(3):
-            print i
+        for i in range(4):
             for row in read_cards:
-                print i
                 for col in row:
                     if c==0:
                         self.chiligrid.add_card(ChiliWordCard(text=col))
