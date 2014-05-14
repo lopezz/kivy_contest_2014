fa5556650787fc91574ea8946d73c51b8d76bbcb
lopezz <lopezcardona24@gmail.com>
2014-05-13 23:55:22 -0500

    See #21 changed cards colors and style

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 MAX_FLIPPED_CARDS = 3 # max flipped cards by user before a check is performed
 # COLOR constants
 NONE_COLOR = [0, 0, 0, 0]
-OBJECT_COLOR = [1, 0, 0, 1]
-WORD_COLOR = [0, 1, 0, 1]
-SOUND_COLOR = [0, 0, 1, 1]
+OBJECT_COLOR = [170/255., 0/255., 90/255., 1] 
+WORD_COLOR = [229/255., 103/255., 23/255., 1]
+SOUND_COLOR = [23/255., 150/255., 230/255, 1]
 UNFLIPPED, FLIPPED, GUESSED = 0, 1, 2
 BGCOLOR_NORMAL = [0, 0, 0, 0]
-BGCOLOR_GUESSED = [0, 1, 0, 0.8]
+BGCOLOR_GUESSED = [31/255., 139/255., 34/255., 0.5]
 STOPPED, RUNNING, PAUSED = 0, 1, 2
